7580e71921d552ff0145bc58bdb7cf2557aa7822
Gideon-Stein <67389361+Gideon-Stein@users.noreply.github.com>
2025-02-17 10:33:31 +0000

    Deploying to gh-pages from @ Gideon-Stein/Gideon-Stein.github.io@d84ac7b491d1bebce2539c86b86b229100862227 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -473,13 +473,6 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=Y4bL1asAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://causalrivers.github.io/", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
